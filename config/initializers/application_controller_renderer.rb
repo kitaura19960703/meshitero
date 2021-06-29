211246d363a6f06c05d22497e8bac99aa63064dc
kitaura19960703 <kitaura19960703@icloud.com>
2021-06-29 06:19:09 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'a3c2863f854f847c741f6cd3566b9bf2bf991a0c88a8896747a53cfc73ec4f98e9455ba3cb7eac2e862b64a29db8750a3eb4f8c5929f40ed66f5a5c230234d0a'
